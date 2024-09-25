82e0b8d075c32cbd626b54d3eca2f416ff46bdc6
kush-ankit <er.ankitkush@gmail.com>
2024-09-25 19:17:08 +0530

    docker added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
+    
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,6 +49,7 @@ function Home() {
       <footer className="bg-gray-200 w-full p-4 text-center">
         <p className="text-gray-600">&copy; 2024 My Blog. All rights reserved.</p>
       </footer>
+      <div>Hello</div>
     </div>
   );
 };
