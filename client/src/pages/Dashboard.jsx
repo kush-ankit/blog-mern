e2ead7789a12905359cf5410262a82b95c21db74
srmcemudit <tudrishu@gmail.com>
2024-10-02 20:08:21 +0530

    daahboard complete

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -20,23 +20,23 @@ function Dashboard() {
 
   return (
     <div className=''>
-      <header className='h-[80vh] w-[calc(100vw-18px)] flex-col justify-center items-center'>
-      <div className='w-[full] bg-black h-[30%] flex justify-center items-center z-10'>
-        <div className='bg-orange-300 h-[180px] w-[180px] px-1 py-1 rounded-full text-black text-center flex justify-center items-center z-20 border-black outline-[10px] outline'>
+      <header className='h-[25rem] w-[calc(100vw-18px)] flex-col justify-center items-center'>
+      <div className='w-full bg-black h-[10rem] flex justify-center items-center z-10'>
+        <div className='bg-orange-300 h-[145px] w-[145px] px-1 py-1 rounded-full mt-4 text-black text-center flex justify-center items-center z-20 border-black outline-[8px] outline'>
         user-image
         </div>
       </div>
-        <div className='bg-slate-200 w-1/2 h-[35%] translate-x-[50%] text-center -mt-10 z-10 p-2 rounded-md '>
+        <div className='bg-slate-200 w-[40%] h-auto translate-x-[76%] -mt-4 text-center z-10 p-2 rounded-md '>
         <p className='mt-5 font-bold text-2xl'>UserName</p>
         <p className='p-2 font-bold'>bio- i am feeling good. </p>
         <p>date of joining - dd/mm/yyyy</p>
-        <div className='flex justify-evenly p-2 '>
+        <div className='flex justify-evenly p-2 font-medium mt-1 '>
           <span>0 posts</span> <span>0 commments</span> <span>0 likes</span>
         </div>
         </div>
       </header>
       <main>
-        <div className='grid p-4'>
+        <div className='grid grid-cols-3 p-2 w-full gap-x-2 gap-y-4 '>
           {/* <h2>Your Blogs</h2>
           {blogs.map(blog => (
             <div key={blog._id}>
@@ -44,6 +44,9 @@ function Dashboard() {
               <p>{blog.status}</p>
             </div>
           ))} */}
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
           <BlogCard />
           <BlogCard />
           <BlogCard />
