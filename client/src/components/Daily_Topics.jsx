477c60cbf5fba8ca29a62cd167a75667fc5c9e11
srmcemudit <tudrishu@gmail.com>
2024-10-14 23:48:45 +0530

    goog

--- client/src/components/Daily_Topics.jsx
+++ client/src/components/Daily_Topics.jsx
@@ -1,24 +1,92 @@
-import React from 'react'
+import { Card, List, Typography, Chip } from "@material-tailwind/react";
+import { GrTechnology } from "react-icons/gr";
+import {Archive,EmptyPage,Mail,Pin,SendDiagonal,Bin,} from "iconoir-react";
+import {MdOutlineLocalMovies } from "react-icons/md";
 
-function Daily_Topics() {
+const Links = [
+  {
+    icon: "<GrTechnology />",
+
+    title: "#Tech",
+
+    href: "#",
+
+    badge: 14,
+  },
+
+  {
+    icon: SendDiagonal,
+
+    title: "#new",
+
+    href: "#",
+
+    badge: 3,
+  },
+
+  {
+    icon: EmptyPage ,
+
+    title: "#movies",
+
+    href: "#",
+  },
+
+  {
+    icon: Pin,
+
+    title: "#connect",
+
+    href: "#",
+  },
+
+  {
+    icon: Archive,
+
+    title: "#bollywood",
+
+    href: "#",
+  },
+
+  {
+    icon: Bin,
+
+    title: "#science",
+
+    href: "#",
+  },
+];
+
+console.log(GrTechnology)
+  function Daily_Topics() {
   return (
-    <div className='flex justify-center p-4'>
-        <section className='w-full rounded-xl shadow-lg ' >
-            <div className='text-center font-bold text-2xl ' >
-                <p>Daily Topics</p>
-            </div>
-            <div className='flex flex-col justify-start items-start text-start p-4 ' >
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2 ' >Movies</span>
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2'>Games</span>
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2'>Science</span>
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2'>Fiction</span>
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2'>News</span>
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2'>Comedy</span>
-                <span className='hover:bg-blue-200 w-full rounded-md hover:p-2'>Music</span>
-            </div>
-        </section>
-    </div>
-  )
-}
+    <Card className="max-w-[280px]">
+      <Card.Header className="mx-4 mb-0 mt-3 h-max">
+        <Typography className="font-bold text-xl ">My Tags</Typography>
+      </Card.Header>
 
+      <Card.Body className="p-3">
+        <List className="hover:cursor-pointer"  >
+          {Links.map(({ icon: Icon, title, href, badge }) => (
+            <List.Item key={title} href={href} className="hover:text-blue-600 hover:font-semibold " >
+              <List.ItemStart >
+                
+              </List.ItemStart>
+
+              {title}
+
+              {badge && (
+                <List.ItemEnd>
+                  <Chip size="sm" variant="ghost" color="primary" >
+                    <Chip.Label>{badge}</Chip.Label>
+                  </Chip>
+                </List.ItemEnd>
+              )}
+            </List.Item>
+          ))}
+        </List>
+      </Card.Body>
+    </Card>
+  );
+}
 export default Daily_Topics
